fe7f682a6ffe2803a177ba56eb5517e895224f19
dorim1209 <dorim1209@gmail.com>
2024-09-05 22:41:19 +0900

    feat: 6.5 블로그 글 조회 API 구현하기

--- src/main/java/com/example/springbootdeveloper/service/BlogService.java
+++ src/main/java/com/example/springbootdeveloper/service/BlogService.java
@@ -14,12 +14,17 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    public Article save(AddArticleRequest request){
+    public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
     }
 
-    public List<Article> findAll(){
-         return blogRepository.findAll();
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 
 }
